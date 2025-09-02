9cbd11030b200670a2fdcb9bf49e655feb90e5a1
EyimofeCole <153179802+EyimofeGitHub@users.noreply.github.com>
2025-09-02 13:48:11 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+discription: This program will allow a simple welcome message to the user
+name: Eyimofe cole
+date:1/9/2025
+Version:1.0
+Notes:n/a
+
+ */
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args)
+        {
+        System.out.print("Hello World");
+        System.out.println("World");
+        
+        System.exit(0);
+        }
+    
+}
