b0c4d2f55eb0436b9f8e2b473432a5de2dddb0f0
EyimofeCole <153179802+EyimofeGitHub@users.noreply.github.com>
2025-09-02 13:49:33 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,9 +13,9 @@ public class HelloWorld {
     public static void main(String[] args)
         {
         System.out.print("Hello World");
-        System.out.println("World");
         
         System.exit(0);
         }
     
 }
+
